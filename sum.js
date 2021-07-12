21f7cb8bb72d98ba68a62428cc08ce5aaff393e1
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-07-12 10:20:06 +0530

    Update sum.js

--- sum.js
+++ sum.js
@@ -1,5 +1,10 @@
 const sum = (a, b) => {
-    return a + b;
+    if (typeof(a) === 'number' && typeof(b) === 'number'){
+        return a + b;
+    }
+    else {
+        throw "pass numbers"
+    }
 }
 
 module.exports = sum;
